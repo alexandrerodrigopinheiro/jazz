f51cbe4443286c13cc3f7eca69b9403d3aa2dd14
Alexandre Rodrigo Pinheiro <alexandrerodrigopinheiro@gmail.com>
2024-10-18 00:31:54 -0300

    Added nitialization

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,21 +1,51 @@
 // main.go
 package main
 
-import "jazz/backend/pkg/logger"
+import (
+	"fmt"
+	"jazz/backend/configs"
+	"jazz/backend/pkg/cache"
+	"jazz/backend/pkg/database"
+	"jazz/backend/pkg/logger"
+
+	"gorm.io/gorm"
+)
 
 func main() {
-	// Inicializa o logger
+	// Inicializa o logger antes de qualquer coisa
 	logger.InitializeLogger()
 
 	// Usa o logger global para logar informações iniciais da aplicação
 	logger.Logger.Info("Application has started")
 
-	// Chama outros módulos
-	runApplication()
+	// Carrega as configurações
+	if err := configs.LoadConfig(); err != nil {
+		panic(fmt.Sprintf("Error loading config: %v", err))
+	}
+
+	// Inicializa o banco de dados (Singleton)
+	db := database.InitializeDatabase()
+
+	// Inicializa o cache (Singleton)
+	cacheManager := cache.NewCacheManager()
+
+	// Testando os serviços
+	runApplication(db, cacheManager)
 }
 
-// Função exemplo para rodar a aplicação
-func runApplication() {
+func runApplication(_ *gorm.DB, cache cache.Cache) {
+	// Executa a lógica principal da aplicação
 	logger.Logger.Info("Running application logic")
-	// ... outras lógicas da aplicação
+
+	// Exemplo de uso do cache e do banco de dados
+	key := "example_key"
+	value := "example_value"
+
+	// Salvando no cache
+	cache.Set(key, value, 60)
+	logger.Logger.Infof("Value '%s' set in cache with key '%s'", value, key)
+
+	// Consultando o banco de dados (exemplo fictício)
+	// logger.Logger.Infof("Database connection details: %v", db)
+	// Outras lógicas da aplicação...
 }
